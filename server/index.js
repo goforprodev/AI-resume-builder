6c6fc451d94c2af93b487ce8c4251c7b0967892c
goforprodev <goforprodev@gmail.com>
2023-02-27 13:05:57 +0100

    added image uploads with  multer

--- server/index.js
+++ server/index.js
@@ -1,10 +1,48 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
+import multer from "multer";
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-const app = express()
+const app = express();
+app.use("/uploads", express.static("uploads"));
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
+
+app.post("/resume/create", upload.single("headShotImage"), async (req, res) => {
+  const {
+    fullName,
+    currentPosition,
+    currentLength,
+    currentTechnologies,
+    workHistory,
+  } = req.body;
+
+  console.log(req.body);
+
+  res.json({
+    message: "Resume created successfully",
+    data: {},
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
